f2956f8ce28bdb1a8f02cd1ffd42447bfaed805b
Val <valeriy.rotan@gmail.com>
2019-01-05 22:18:33 -0800

    initial commit

---
+++ src/main/java/frc/team972/robot/Robot.java
@@ -0,0 +1,23 @@
+package frc.team972.robot;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+
+public class Robot extends TimedRobot {
+
+  @Override
+  public void robotInit() {
+  }
+
+  @Override
+  public void robotPeriodic() {
+  }
+
+  @Override
+  public void autonomousInit() {
+  }
+  
+  @Override
+  public void teleopInit() {
+  }
+  
+}
