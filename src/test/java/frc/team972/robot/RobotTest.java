2e825de5348fbcf2dc6223fec17f148511b75dd5
Val <valeriy.rotan@gmail.com>
2019-01-06 11:33:40 -0800

    added unit tests

---
+++ src/test/java/frc/team972/robot/RobotTest.java
@@ -0,0 +1,14 @@
+package frc.team972.robot;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RobotTest {
+
+	@Test
+	public void test() {
+		assertTrue("test", true);
+	}
+
+}
