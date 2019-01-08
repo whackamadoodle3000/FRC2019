36cc0440b5e94ee311f86b32380aa688f5651531
Jody7 <jody7777pvp@gmail.com>
2019-01-07 23:25:56 -0500

    Add CTRE and Talon Drivers

---
+++ src/main/java/frc/team972/robot/subsystems/Drive.java
@@ -0,0 +1,19 @@
+package frc.team972.robot.subsystems;
+
+public class Drive extends Subsystem {
+
+    @Override
+    public boolean checkSystem() {
+        return false;
+    }
+
+    @Override
+    public void outputTelemetry() {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+}
