f69a6d735664754807044d78b263099194d06247
Pranav <orca.pranav@gmail.com>
2019-01-19 15:17:41 -0800

    Changed Arm Code

--- src/main/java/frc/team972/robot/subsystems/Arm.java
+++ src/main/java/frc/team972/robot/subsystems/Arm.java
@@ -1,24 +1,37 @@
 package frc.team972.robot.subsystems;
 
-import frc.team972.robot.driver_utils.TalonSRXFactory;
 import frc.team972.robot.loops.ILooper;
+import frc.team972.robot.driver_utils.TalonSRXFactory;
 import edu.wpi.first.wpilibj.Encoder;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
 public class Arm extends Subsystem {
-    private static final double ClicksToDegrees = 1;
     private static Arm mArm = new Arm();
-    private static TalonSRXFactory mArmTalon = new TalonSRXFactory(1);
+
+    //Arm initializations
+    public static double desiredArmAngle = 0;
+    private static double currentArmAngle = 0;
+    public static final double clicksToDegrees = 1;
     private static Encoder armEncoder = new Encoder(0, 1, false, Encoder.EncodingType.k4X);
+    private static boolean isFirstTime = true;
 
-    public static armDegrees = 0;
-    public static desiredArmDegrees = 0;
+    //PID initializations
+    private static final double proportionFactor = 0;
+    private static final double integralFactor = 0;
+    private static final double derivativeFactor = 0;
+    private static double currentValue = 0;
+    private static double previousValue = 0;
+    private static double integral = 0;
 
+    public static TalonSRX armTalon = TalonSRXFactory.createDefaultTalon(1);
 
-    public void setDesiredArmDegrees(double degrees){
-        desiredArmDegrees = degrees;
+    public double getCurrentArmAngle(){
+        return currentArmAngle;
     }
-    public double getArmDegrees(){
-        return armDegrees;
+    public void initialize(){
+        zeroSensors();
+        armEncoder.setDistancePerPulse(clicksToDegrees);
+        isFirstTime = false;
     }
     public void writeToLog() {
     }
@@ -27,25 +40,50 @@ public class Arm extends Subsystem {
     }
 
     public void writePeriodicOutputs() {
-
+        if (isFirstTime){
+            initialize();
+        }
+        moveToAngle();
         //System.out.println("Test");
+    }
+
+    public void moveToAngle(){
+        previousValue = currentValue;
+        currentValue = armEncoder.get();
+        if ((desiredArmAngle-currentArmAngle)<1.5){
+            integral=0;
+        }
+        else {
+            integral = integral + currentValue - desiredArmAngle;
+        }
+        armTalon.set(proportionFactor * (desiredArmAngle - currentValue) + integralFactor * (integral) + derivativeFactor * (currentValue - previousValue));
+
     }
 
     public boolean checkSystem() {
         return true;
     }
 
-
     public void outputTelemetry() {
     }
 
     public void stop() {
+        armEncoder.reset();
     }
 
     public void zeroSensors() {
+        armEncoder.reset();
     }
 
     public void registerEnabledLoops(ILooper enabledLooper) {
+    }
+
+    public void setDesiredArmAngle(double angle){
+        desiredArmAngle = angle;
+    }
+
+    public double getDesiredArmAngle(){
+        return desiredArmAngle;
     }
 
     public static ExampleSubsystem getInstance() {
