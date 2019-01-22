1c698e327376e8771d2586a050e379f0ef3b2185
whackamadoodle3000 <orca.pranav@gmail.com>
2019-01-22 15:28:58 -0800

    added pid class usage

--- src/main/java/frc/team972/robot/subsystems/Arm.java
+++ src/main/java/frc/team972/robot/subsystems/Arm.java
@@ -2,40 +2,39 @@ package frc.team972.robot.subsystems;
 
 import frc.team972.robot.loops.ILooper;
 import frc.team972.robot.driver_utils.TalonSRXFactory;
+import frc.team972.robot.util.PIDHandler;
 import edu.wpi.first.wpilibj.Encoder;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
 public class Arm extends Subsystem {
     //General initializations
     private static Arm mArm = new Arm();
-    private static boolean isFirstTime = true;
     private static boolean everythingStopped = false;
     
     //Arm motor and encoder initializations
     public static double desiredArmAngle = 0;
     private static double currentArmAngle = 0;
     public static final double clicksToDegrees = 1;
-    private static Encoder armEncoder = new Encoder(0, 1, false, Encoder.EncodingType.k4X);
-    private static final double[] angleArray = [20,30,40]; //different set heights
+    private static Encoder pArmEncoder = new Encoder(0, 1, false, Encoder.EncodingType.k4X);
+    private static final double[] angleArray = new double[] {20,30,40,50}; //different set heights
 
     //PID initializations
-    private static final double proportionFactor = 0;
-    private static final double integralFactor = 0;
-    private static final double derivativeFactor = 0;
-    private static double currentValue = 0;
-    private static double previousValue = 0;
-    private static double integral = 0;
+    private final double kProportion = 0;
+    private final double kIntegral = 0;
+    private final double kDerivative = 0;
+    private PIDHandler PIDControl = new PIDHandler(angleArray[angleArray.length-1], kProportion, kIntegral, kDerivative, 0);
 
     public static TalonSRX armTalon = TalonSRXFactory.createDefaultTalon(1);
+
+    public Arm (){
+        zeroSensors();
+        pArmEncoder.setDistancePerPulse(clicksToDegrees);
+    }
 
     public double getCurrentArmAngle(){
         return currentArmAngle;
     }
-    public void initialize(){
-        zeroSensors();
-        armEncoder.setDistancePerPulse(clicksToDegrees);
-        isFirstTime = false;
-    }
+
     public void writeToLog() {
     }
 
@@ -43,27 +42,13 @@ public class Arm extends Subsystem {
     }
 
     public void writePeriodicOutputs() {
-        if (isFirstTime){
-            initialize();
-        }
+
         if (!everythingStopped){
-            currentArmAngle = armEncoder.get();
-            moveToAngle();
+            currentArmAngle = pArmEncoder.get();
+            PIDControl.setDesired(desiredArmAngle);
+            armTalon.set(PIDControl.output(currentArmAngle));
         }
         //System.out.println("Test");
-    }
-
-    public void moveToAngle(){
-        previousValue = currentValue;
-        currentValue = armEncoder.get();
-        if ((desiredArmAngle-currentArmAngle)<1.5){
-            integral=0;
-        }
-        else {
-            integral = integral + currentValue - desiredArmAngle;
-        }
-        armTalon.set(proportionFactor * (desiredArmAngle - currentValue) + integralFactor * (integral) + derivativeFactor * (currentValue - previousValue));
-
     }
 
     public boolean checkSystem() {
@@ -78,8 +63,12 @@ public class Arm extends Subsystem {
         everythingStopped = true;
     }
 
+    public void start() {
+        everythingStopped = false;
+    }
+
     public void zeroSensors() {
-        armEncoder.reset();
+        pArmEncoder.reset();
     }
 
     public void registerEnabledLoops(ILooper enabledLooper) {
@@ -89,7 +78,7 @@ public class Arm extends Subsystem {
         desiredArmAngle = angle;
     }
     
-    public void setDesiredArmLevel(double level){
+    public void setDesiredArmLevel(int level){
         desiredArmAngle = angleArray[level];
     }
 
@@ -97,7 +86,7 @@ public class Arm extends Subsystem {
         return desiredArmAngle;
     }
 
-    public static ExampleSubsystem getInstance() {
-        return mInstance;
+    public static Arm getInstance() {
+        return mArm;
     }
 }
